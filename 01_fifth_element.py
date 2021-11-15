0ba1f580ad9a929ce61e421a3af64e79147a70c1
EVG <evgeniy-kovtyak@mail.ru>
2021-11-15 17:57:12 +0300

    01 completed. idk how to check the last exception...

--- 01_fifth_element.py
+++ 01_fifth_element.py
@@ -5,9 +5,17 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+except ValueError as exc:
+    print(f'Your input is not numeric...{exc}')
+except IndexError as exc:
+    print(f'Your string is too short...{exc}')
+except Exception as exc:
+    print(f'Something is wrong...{exc}')
+else:
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
